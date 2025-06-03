f2beed9a704f1817263c41b3dc373f365358f333
Vinicius Nobre <vina.santos06@gmail.com>
2025-06-03 16:23:17 -0300

    Add files via upload

--- src/userList.jsx
+++ src/userList.jsx
@@ -3,26 +3,65 @@ import './userList.module.css'
 import { useEffect, useState } from 'react'
 import { Menu } from './components/menu'
 import { api } from './api/api'
+import { useNavigate } from 'react-router'
 
 function UserList() {
+    const navigate = useNavigate()
     const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState([])
+    const [loading, setLoading] = useState(true)
     const [error, setError] = useState('')
+    const [editUserId, setEditUserId] = useState(null)
+    const [editData, setEditData] = useState({name: '', email: '', password: ''})
 
     useEffect(() => {
-        async function fetchUsers(){
-            try {
-                const response = await api.get('/users')
-                setUsers(response.data)
-                // console.log(response.data)
-            } catch (error) {
-                setError('Erro ao carregar usuários', error)
-            } finally {
-                setLoading(false)
-            }
+        const storedUser = localStorage.getItem('user')
+        if(!storedUser) navigate('/')
+    }, [navigate])
+
+    const fetchUsers = async () =>{
+        try {
+            const response = await api.get('/users')
+            setUsers(response.data)
+            // console.log(response.data)
+        } catch (error) {
+            setError('Erro ao carregar usuários', error)
+        } finally {
+            setLoading(false)
         }
+    }
+        
+    useEffect(() => {
         fetchUsers()
     }, [])
+    
+    const handleDelete = async (id) => {
+        try {
+            await api.delete(`/users/${id}`)
+            setUsers(users.filter((u) => u.id !== id))
+        } catch(err) {
+            setError('Erro ao deletar usuario', err) 
+        }
+    }
+
+    const handleEditClick = (user) => {
+        setEditUserId(user.id)
+        setEditData({name: user.name, email: user.email, password: ''})// não mostra senha antiga
+    }
+
+    const handleEditChange = (e) => {
+        const {name, value} = e.target
+        setEditData({...editData, [name]: value})
+    }
+
+    const handleUpdate = async (e) => {
+        e.preventDefault()
+        try {
+            await api.put(`/users/${editUserId}`, editData)
+            setEditUserId(null)
+        } catch (err) {
+            setError('Erro ao atualizar usuario', err)
+        }
+    }
 
     if (loading) return <p>Carregando usuarios...</p>
     if (error) return <p>{error}</p>
@@ -32,13 +71,29 @@ function UserList() {
             <Menu/>
             <div style={{padding: '2rem'}}>
                 <h1>Lista de usuários</h1>
-                <ul>
-                    {users.map((item) => (
-                        <li key={item.id}>
-                        <strong>{item.name}</strong> - <i>{item.email}</i>
+                <ol>
+                    {users.map((user) => (
+                        <li key={user.id} style={{marginTop: '2rem', marginLeft: '1rem'}}>
+                            {editUserId === user.id ? (
+                                <form onSubmit={handleUpdate} style={{display: 'flex', flexDirection: 'row', gap: '0.5rem'}}>
+                                    <input type="text" name='name' value={editData.name} onChange={handleEditChange} required/>
+                                    <input type="email" name='email' value={editData.email} onChange={handleEditChange} required/>
+                                    <input type="password" name='password' value={editData.password} onChange={handleEditChange} placeholder='Nova senha' required/>
+                                    <button type='submit'>Salvar</button>
+                                    <button type='button' onClick={() => setEditUserId(null)}>Cancelar</button>
+                                </form>
+                            ) : (
+                                <>
+                                    <strong>{user.name}</strong> - <i>{user.email}</i>
+                                    <div style={{display: 'inline-flex', gap: '0.5rem', marginLeft: '1rem'}}>
+                                        <button onClick={() => handleEditClick(user)}>EDITAR</button>
+                                        <button onClick={() => handleDelete(user.id)}>DELETAR</button>
+                                    </div>
+                                </>
+                            )}
                     </li>
                     ))}
-                </ul>
+                </ol>
             </div>
         </section>
     )
